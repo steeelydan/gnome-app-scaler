d4dd08b5583c7854e535183f3a583f934f38b969
Steeely Dan <steeelydan@posteo.de>
2021-11-11 17:51:47 +0100

    Check if scale setting is configured

--- scaler.py
+++ scaler.py
@@ -25,17 +25,30 @@ for preset_name in settings:
     if chosen_preset == preset_name:
         for app_name in settings[preset_name]:
             if app_name == 'gnome':
-                font_scale = settings[chosen_preset]['gnome']['font_scale']
-                icon_size = settings[chosen_preset]['gnome']['icon_size']
-                cursor_size = settings[chosen_preset]['gnome']['cursor_size']
+                gnome_settings = settings[chosen_preset]['gnome']
 
-                print("Gnome font scale set to %s" % font_scale) if 0 == os.system(
-                    "gsettings set org.gnome.desktop.interface text-scaling-factor %s" % font_scale) else error("Error: Gnome font scale could not be set to %s" % font_scale)
+                font_scale = 'font_scale' in gnome_settings and gnome_settings['font_scale']
+                icon_size = 'icon_size' in gnome_settings and gnome_settings['icon_size']
+                cursor_size = 'cursor_size' in gnome_settings and gnome_settings['cursor_size']
 
-                print("Gnome icon size set to %s px" % icon_size) if 0 == os.system(
-                    "gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size %s" % icon_size) else error("Error: Gnome icon size could not be set to %s" % icon_size)
+                if font_scale:
+                    if 0 == os.system("gsettings set org.gnome.desktop.interface text-scaling-factor %s" % font_scale):
+                        print("Gnome font scale set to %s" % font_scale)
+                    else:
+                        error("Error: Gnome font scale could not be set to %s" %
+                              font_scale)
 
-                print("Gnome cursor size set to %s px" % cursor_size) if 0 == os.system(
-                    "gsettings set org.gnome.desktop.interface cursor-size %s" % cursor_size
-                ) else error("Error: Gnome cursor size could not be set to %s" % cursor_size)
+                if icon_size:
+                    if 0 == os.system("gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size %s" % icon_size):
+                        print("Gnome icon size set to %s px" % icon_size)
+                    else:
+                        error("Error: Gnome icon size could not be set to %s" %
+                              icon_size)
+
+                if cursor_size:
+                    if 0 == os.system("gsettings set org.gnome.desktop.interface cursor-size %s" % cursor_size):
+                        print("Gnome cursor size set to %s px" % cursor_size)
+                    else:
+                        error("Error: Gnome cursor size could not be set to %s" %
+                              cursor_size)
         break
