b31f1e251e79d87896a41bfe3d347525e5208367
Steeely Dan <steeelydan@posteo.de>
2021-11-12 13:36:23 +0100

    Use modern string formatting

--- scaler.py
+++ scaler.py
@@ -36,31 +36,26 @@ with open('settings.json', 'r') as settings_file:
                     cursor_size = 'cursor_size' in gnome_settings and gnome_settings['cursor_size']
 
                     if font_scale:
-                        if 0 == os.system("gsettings set org.gnome.desktop.interface text-scaling-factor %s" % font_scale):
-                            print("Gnome font scale set to %s" % font_scale)
+                        if 0 == os.system(f"gsettings set org.gnome.desktop.interface text-scaling-factor {font_scale}"):
+                            print(f"Gnome font scale set to {font_scale}")
                         else:
-                            error("Error: Gnome font scale could not be set to %s" %
-                                  font_scale)
+                            error(f"Error: Gnome font scale could not be set to {font_scale}")
 
                     if icon_size:
-                        if 0 == os.system("gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size %s" % icon_size):
-                            print("Gnome icon size set to %s px" % icon_size)
+                        if 0 == os.system(f"gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size {icon_size}"):
+                            print(f"Gnome icon size set to {icon_size} px")
                         else:
-                            error("Error: Gnome icon size could not be set to %s" %
-                                  icon_size)
+                            error(f"Error: Gnome icon size could not be set to {icon_size}")
 
                     if cursor_size:
-                        if 0 == os.system("gsettings set org.gnome.desktop.interface cursor-size %s" % cursor_size):
-                            print("Gnome cursor size set to %s px" %
-                                  cursor_size)
+                        if 0 == os.system(f"gsettings set org.gnome.desktop.interface cursor-size {cursor_size}"):
+                            print(f"Gnome cursor size set to {cursor_size} px")
                         else:
-                            error("Error: Gnome cursor size could not be set to %s" %
-                                  cursor_size)
+                            error(f"Error: Gnome cursor size could not be set to {cursor_size}")
 
                 if app_name == 'firefox':
                     firefox_config = config['firefox']
-                    firefox_profile_dir = "%s/.mozilla/firefox/%s/" % (
-                        Path.home(), firefox_config['profile_folder'])
+                    firefox_profile_dir = f"{Path.home()}/.mozilla/firefox/{firefox_config['profile_folder']}/"
                     user_js_path = firefox_profile_dir + 'user.js'
                     firefox_settings = presets[chosen_preset]['firefox']
 
