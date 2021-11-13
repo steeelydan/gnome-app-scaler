b621d01fb015cb62961950044dcc5373602d47fc
Maximilian Michels <mxm@apache.org>
2021-11-13 15:21:00 +0100

    Pythonic refactoring

--- scaler.py
+++ scaler.py
@@ -12,89 +12,91 @@ def error(message):
     sys.exit(1)
 
 
-if len(sys.argv) < 2:
-    error('Error: You did not specify a preset')
+def apply_gnome(gnome_settings):
+    font_scale = 'font_scale' in gnome_settings and gnome_settings['font_scale']
+    icon_size = 'icon_size' in gnome_settings and gnome_settings['icon_size']
+    cursor_size = 'cursor_size' in gnome_settings and gnome_settings['cursor_size']
+
+    if font_scale:
+        if 0 == os.system(f"gsettings set org.gnome.desktop.interface text-scaling-factor {font_scale}"):
+            print(f"Gnome font scale set to {font_scale}")
+        else:
+            error(
+                f"Error: Gnome font scale could not be set to {font_scale}")
+
+    if icon_size:
+        if 0 == os.system(f"gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size {icon_size}"):
+            print(f"Gnome icon size set to {icon_size} px")
+        else:
+            error(
+                f"Error: Gnome icon size could not be set to {icon_size}")
+
+    if cursor_size:
+        if 0 == os.system(f"gsettings set org.gnome.desktop.interface cursor-size {cursor_size}"):
+            print(f"Gnome cursor size set to {cursor_size} px")
+        else:
+            error(
+                f"Error: Gnome cursor size could not be set to {cursor_size}")
 
 
-with open('settings.json', 'r') as settings_file:
-    settings_parsed = json.loads(settings_file.read())
-    presets = settings_parsed['presets']
-    config = settings_parsed['config']
+def apply_firefox(firefox_settings, firefox_config):
+    firefox_profile_dir = f"{Path.home()}/.mozilla/firefox/{firefox_config['profile_folder']}/"
+    user_js_path = firefox_profile_dir + 'user.js'
+
+    new_line = f"user_pref('layout.css.devPixelsPerPx', '{firefox_settings['scale']}');\n"
+
+    lines = []
+
+    if not os.path.isfile(user_js_path):
+        user_js_raw = open(user_js_path, 'x')
+        lines.append(new_line)
+    else:
+        user_js_raw = open(user_js_path, 'r')
+        lines = user_js_raw.readlines()
+        if len(lines) == 0:
+            lines.append(new_line)
+        else:
+            for index, line in enumerate(lines):
+                if line.startswith('user_pref'):
+                    lines[index] = new_line
+    user_js_raw.close()
+    user_js_raw = open(user_js_path, 'w')
+    user_js_raw.writelines(lines)
+    print(
+        f"Firefox scaling set to {firefox_settings['scale']}")
+
+
+def apply_chromium(chromium_settings):
+    chromium_path = '/var/lib/snapd/desktop/applications/chromium_chromium.desktop'
+    chromium_result = subprocess.run(
+        ['sudo', 'sed',  '-i', f"s/\/snap\/bin\/chromium.*%U/\/snap\/bin\/chromium --force-device-scale-factor={chromium_settings['scale']} %U/", '/var/lib/snapd/desktop/applications/chromium_chromium.desktop'])
+    if chromium_result.returncode == 0:
+        print(
+            f"Chromium scaling set to {chromium_settings['scale']}")
+    else:
+        error(
+            f"Error setting chromium scale in: {chromium_path}")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        error('Error: You did not specify a preset')
 
     chosen_preset = sys.argv[1]
 
-    if not chosen_preset in presets:
-        error('Error: Preset does not exist.')
+    with open('settings.json', 'r') as settings_file:
+        settings_parsed = json.loads(settings_file.read())
+        presets = set(settings_parsed['presets'])
+        config = settings_parsed['config']
 
-    for preset_name in presets:
-        if chosen_preset == preset_name:
-            for app_name in presets[preset_name]:
-                if app_name == 'gnome':
-                    gnome_settings = presets[chosen_preset]['gnome']
+        if chosen_preset not in presets:
+            error('Error: Preset does not exist.')
 
-                    font_scale = 'font_scale' in gnome_settings and gnome_settings['font_scale']
-                    icon_size = 'icon_size' in gnome_settings and gnome_settings['icon_size']
-                    cursor_size = 'cursor_size' in gnome_settings and gnome_settings['cursor_size']
-
-                    if font_scale:
-                        if 0 == os.system(f"gsettings set org.gnome.desktop.interface text-scaling-factor {font_scale}"):
-                            print(f"Gnome font scale set to {font_scale}")
-                        else:
-                            error(
-                                f"Error: Gnome font scale could not be set to {font_scale}")
-
-                    if icon_size:
-                        if 0 == os.system(f"gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size {icon_size}"):
-                            print(f"Gnome icon size set to {icon_size} px")
-                        else:
-                            error(
-                                f"Error: Gnome icon size could not be set to {icon_size}")
-
-                    if cursor_size:
-                        if 0 == os.system(f"gsettings set org.gnome.desktop.interface cursor-size {cursor_size}"):
-                            print(f"Gnome cursor size set to {cursor_size} px")
-                        else:
-                            error(
-                                f"Error: Gnome cursor size could not be set to {cursor_size}")
-
-                if app_name == 'firefox':
-                    firefox_config = config['firefox']
-                    firefox_profile_dir = f"{Path.home()}/.mozilla/firefox/{firefox_config['profile_folder']}/"
-                    user_js_path = firefox_profile_dir + 'user.js'
-                    firefox_settings = presets[chosen_preset]['firefox']
-
-                    new_line = f"user_pref('layout.css.devPixelsPerPx', '{firefox_settings['scale']}');\n"
-
-                    lines = []
-
-                    if not os.path.isfile(user_js_path):
-                        user_js_raw = open(user_js_path, 'x')
-                        lines.append(new_line)
-                    else:
-                        user_js_raw = open(user_js_path, 'r')
-                        lines = user_js_raw.readlines()
-                        if len(lines) == 0:
-                            lines.append(new_line)
-                        else:
-                            for index, line in enumerate(lines):
-                                if line.startswith('user_pref'):
-                                    lines[index] = new_line
-                    user_js_raw.close()
-                    user_js_raw = open(user_js_path, 'w')
-                    user_js_raw.writelines(lines)
-                    print(
-                        f"Firefox scaling set to {firefox_settings['scale']}")
-
-                if app_name == 'chromium':
-                    chromium_path = '/var/lib/snapd/desktop/applications/chromium_chromium.desktop'
-                    chromium_settings = presets[chosen_preset]['chromium']
-                    chromium_result = subprocess.run(
-                        ['sudo', 'sed',  '-i', f"s/\/snap\/bin\/chromium.*%U/\/snap\/bin\/chromium --force-device-scale-factor={chromium_settings['scale']} %U/", '/var/lib/snapd/desktop/applications/chromium_chromium.desktop'])
-                    if chromium_result.returncode == 0:
-                        print(
-                            f"Chromium scaling set to {chromium_settings['scale']}")
-                    else:
-                        error(
-                            f"Error setting chromium scale in: {chromium_path}")
-
-            break
+        for app_name in presets[chosen_preset]:
+            if app_name == 'gnome':
+                apply_gnome(presets[chosen_preset]['gnome'])
+            elif app_name == 'firefox':
+                apply_firefox(firefox_settings=presets[chosen_preset]['firefox'],
+                              firefox_config=config['firefox'])
+            elif app_name == 'chromium':
+                apply_chromium(presets[chosen_preset]['chromium'])
